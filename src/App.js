52ee00d63597cd2c7bb0868ba45a609b4ec9060c
Mayuri Gurav <mayurigurav1997@gmail.com>
2022-08-13 23:19:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import { Users } from './users';
+import { useState } from 'react'
+import Table from "./Table"
+
 
 function App() {
+  const [query, setQuery] = useState("");
+  const keys = ["first_name", "last_name", "email"];
+
+  const search = (data) => {
+    // return data.filter(item => item.first_name.toLowerCase().includes(query) || item.last_name.toLowerCase().includes(query) || item.email.toLowerCase().includes(query))
+
+    // The some() method returns true (and stops) if the function returns true for one of the array elements
+    return data.filter(item => keys.some(key => item[key].toLowerCase().includes(query)))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Serach...." className="search" onChange={e => setQuery(e.target.value)} />
+      {/* <ul className="list">
+        {Users.filter(user => user.first_name.toLowerCase().includes(query)).map((user) => {
+          return (<li key={user.id} className="listItem">{user.first_name}</li>)
+        })}
+      </ul> */}
+
+      <Table data={search(Users)} />
     </div>
   );
 }
